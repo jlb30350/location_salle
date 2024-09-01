afec89d31c2027c1b107e0aaa16578d5c93d20b5
jlb <jeanlucbonneville@gmail.com>
2024-09-01 03:45:32 +0200

    calendrier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,13 @@
-
-
-
-
-//= application.js
 //= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require_tree 
-
-
-
-
-
+//= require_tree .
 
 if (typeof jQuery === 'undefined') {
   console.error("jQuery is not loaded. Please ensure jQuery is included.");
 } else {
   console.log("jQuery is loaded correctly.");
 }
-
-
 
 // Initialisation de Lightbox
 function initializeLightbox() {
@@ -114,40 +102,41 @@ document.addEventListener("DOMContentLoaded", function() {
   
   if (calendarEl) {
     const roomId = calendarEl.dataset.roomId;
-    
-   
 
-Calendar.prototype.initialRender = function () {
-  var _this = this;
-  
-  if (!this.el) {
-      console.error("Element 'el' is undefined or null. Ensure that 'el' is properly initialized.");
-      return;
-  }
+    // Initialisation du calendrier (vous pouvez remplacer cette partie par le code d'initialisation de votre calendrier si vous utilisez une bibliothèque spécifique)
+    Calendar.prototype.initialRender = function () {
+      var _this = this;
 
-  var el = this.el;  // Utilisation de l'élément natif
-
-  // Ajout de la classe 'fc' sans jQuery
-  el.classList.add('fc');
-
-  // Délégation des événements pour les liens de navigation
-  el.addEventListener('click', function (ev) {
-      if (ev.target.matches('a[data-goto]')) {
-          var anchorEl = ev.target;
-          var gotoOptions = JSON.parse(anchorEl.getAttribute('data-goto'));  // Parse manuellement le JSON
-          var date = _this.moment(gotoOptions.date);
-          var viewType = gotoOptions.type;
-
-          // Propriété comme "navLinkDayClick". Peut être une chaîne ou une fonction
-          var customAction = _this.view.opt('navLink' + util_1.capitaliseFirstLetter(viewType) + 'Click');
-          if (typeof customAction === 'function') {
-              customAction(date, ev);  // Appelle la fonction de personnalisation
-          } else {
-              if (typeof customAction === 'string') {
-                  viewType = customAction;
-              }
-              _this.zoomTo(date, viewType);  // Effectue un zoom sur la vue appropriée
-          }
+      if (!this.el) {
+          console.error("Element 'el' is undefined or null. Ensure that 'el' is properly initialized.");
+          return;
       }
-  }, false);
-};
+
+      var el = this.el;  // Utilisation de l'élément natif
+
+      // Ajout de la classe 'fc' sans jQuery
+      el.classList.add('fc');
+
+      // Délégation des événements pour les liens de navigation
+      el.addEventListener('click', function (ev) {
+          if (ev.target.matches('a[data-goto]')) {
+              var anchorEl = ev.target;
+              var gotoOptions = JSON.parse(anchorEl.getAttribute('data-goto'));  // Parse manuellement le JSON
+              var date = _this.moment(gotoOptions.date);
+              var viewType = gotoOptions.type;
+
+              // Propriété comme "navLinkDayClick". Peut être une chaîne ou une fonction
+              var customAction = _this.view.opt('navLink' + util_1.capitaliseFirstLetter(viewType) + 'Click');
+              if (typeof customAction === 'function') {
+                  customAction(date, ev);  // Appelle la fonction de personnalisation
+              } else {
+                  if (typeof customAction === 'string') {
+                      viewType = customAction;
+                  }
+                  _this.zoomTo(date, viewType);  // Effectue un zoom sur la vue appropriée
+              }
+          }
+      }, false);
+    };
+  }
+});
