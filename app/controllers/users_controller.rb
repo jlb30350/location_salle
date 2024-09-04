2af13705cf2efbec98d3448411283a8429a56b83
jlb <jeanlucbonneville@gmail.com>
2024-09-05 00:23:45 +0200

    paiement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
-    def switch_role
-      if current_user.client?
-        current_user.update(role: :bailleur)
-        redirect_to dashboard_path, notice: "Vous êtes maintenant en mode bailleur. Vous pouvez maintenant gérer vos propres salles."
-      elsif current_user.bailleur?
-        current_user.update(role: :client)
-        redirect_to dashboard_path, notice: 'Vous êtes maintenant un client.'
-      end
+  def switch_role
+    if current_user.loueur?
+      current_user.update(role: 'bailleur')
+      redirect_to dashboard_path, notice: "Vous êtes maintenant en mode bailleur. Vous pouvez maintenant gérer vos propres salles."
+    elsif current_user.bailleur?
+      current_user.update(role: 'loueur')
+      redirect_to dashboard_path, notice: "Vous êtes maintenant en mode loueur. Vous pouvez maintenant réserver des salles."
+    else
+      redirect_to dashboard_path, alert: "Erreur de changement de rôle."
     end
   end
+end
